66a29e38cf30b2718f1c4b8096ceeb739da14b0d
isabellefeltrin <isalufeltrin@gmail.com>
2025-06-13 09:28:42 -0300

    criando projeto

---
+++ src/components/styles/themes/default.ts
@@ -0,0 +1,16 @@
+import type { DefaultTheme } from 'styled-components'; 
+
+export const defaultTheme: DefaultTheme = {
+    colors: {
+        background: '#FFFFFF',
+        black: '#2E2E2E',
+        'primary': '#081534',
+        'primary-hover':'#010c26',
+        'secondary': '#E9ECEF',
+        'success': '#198754',
+        'error': '#DC3545',
+        'metrics' : '#0D6EFD',
+        'font-primary': '#212529',
+        'font-secondary': '#6C757D',
+      }
+};
